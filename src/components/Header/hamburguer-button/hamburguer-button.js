67aafbf53a626ef16c99723089bf67d6b3e688c2
Danilo <danilomurillo0311@gmail.com>
2022-07-13 09:49:34 +0200

    Se agregó animación al botón de tipo hamburguesa a través de funciones fold y unfold

--- src/components/Header/hamburguer-button/hamburguer-button.js
+++ src/components/Header/hamburguer-button/hamburguer-button.js
@@ -1,7 +1,10 @@
+let hamburguerDefaultState = true;
+const hamburguerElement = document.getElementsByClassName('hamburguer-button-lines');
+
 function HamburguerButton() {
 
     return (
-        <button id="hamburguer-button" onClick={fold}>
+        <button id="hamburguer-button" onClick={hamburguerhandler}>
             <div className="hamburguer-button-lines"></div>
             <div className="hamburguer-button-lines"></div>
             <div className="hamburguer-button-lines"></div>
@@ -12,7 +15,41 @@ function HamburguerButton() {
 
 function fold() {
 
-    console.log('hello world');
+    hamburguerElement[0].style.transition = "0.22s";
+    hamburguerElement[0].style.transform = "rotate(0deg)";
+
+    hamburguerElement[2].style.transform = "rotate(0deg)";
+    hamburguerElement[2].style.marginTop = "15%";
+
+    setTimeout(() => {
+        
+        hamburguerElement[1].style.display = "initial";
+        hamburguerElement[1].style.opacity = "100%";
+
+    }, 100);
+
+    hamburguerDefaultState = true;
+
+}
+
+function unfold() {
+
+    hamburguerElement[0].style.transition = "0.44s";
+    hamburguerElement[0].style.transform = "rotate(-50deg)";
+
+    hamburguerElement[1].style.display = "none";
+    hamburguerElement[1].style.opacity = "0%";
+
+    hamburguerElement[2].style.transform = "rotate(50deg)";
+    hamburguerElement[2].style.marginTop = "-3%";
+
+    hamburguerDefaultState = false;
+
+}
+
+function hamburguerhandler() {
+
+    hamburguerDefaultState === true ? unfold() : fold();
 
 }
 
